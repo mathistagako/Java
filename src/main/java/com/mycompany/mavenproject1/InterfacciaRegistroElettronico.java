4c3a2155171d440cbc74638100098a316925dfae
mathistagako <mathistagako@gmail.com>
2022-10-12 23:22:13 +0200

    Java Registro Elettronico

--- src/main/java/com/mycompany/mavenproject1/InterfacciaRegistroElettronico.java
+++ src/main/java/com/mycompany/mavenproject1/InterfacciaRegistroElettronico.java
@@ -178,6 +178,7 @@ public class InterfacciaRegistroElettronico extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        jTextArea1.setText("");
         String str = jTextField1.getText();
         
         ArrayList<Studente> studentsFound = new ArrayList<Studente>();
